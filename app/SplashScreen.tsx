14fe3ad31b147b98a0bd2cbc9d545e80f561b677
jdeoro <jorgedeoro@yahoo.com.ar>
2024-11-04 15:51:52 -0300

    se cambió el puerto y salió andando en expo

--- app/SplashScreen.tsx
+++ app/SplashScreen.tsx
@@ -5,7 +5,7 @@ import { Link, Stack } from 'expo-router'
 export default function index() {
   return (
       <View className="flex flex-1 items-center justify-center bg-orange-500" >
-        <Image className="flex w-10 px-9" source={require('../assets/delivery-1.webp')} />
+        <Image className="flex px-9 aspect-video" source={require('../assets/delivery-1.webp')} />
         <Text className="text-lg text-white">Bienvenido</Text>
       </View>
   );
